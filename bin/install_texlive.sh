320f4a32ae4c933ab71f8a8f8d8d47fb861b3b5d
Tomoya Yoshioka <tmsick.jp@gmail.com>
2019-01-01 03:56:03 +0900

    Simplify bin/install_texlive.sh

--- bin/install_texlive.sh
+++ bin/install_texlive.sh
@@ -6,33 +6,26 @@ source "$(dirname $0)/.bashrc"
 
 readonly REPO_ROOT="$(cd $(dirname $0) && cd .. && pwd)"
 
-
 # Use JAIST as a mirror site
 readonly TEXLIVE_TLNET_URL="http://ftp.jaist.ac.jp/pub/CTAN/systems/texlive/tlnet"
 
-function install_texlive() {
-  local -r texlive_tlnet_url="$1"
+# Generate a unique tmp working dir
+TMP_DIR="/tmp/texlive_$(date | md5)"
+while [[ -d "$TMP_DIR" ]]; do
+  TMP_DIR="/tmp/texlive_$(echo $TMP_DIR | md5)"
+done
+declare -r TMP_DIR
+mkdir -p "$TMP_DIR"
+cd "$TMP_DIR"
 
-  # Generate a unique tmp working dir
-  local tmp_dir="/tmp/texlive_$(date | md5)"
-  while [[ -d "$tmp_dir" ]]; do
-    tmp_dir="/tmp/texlive_$(echo $tmp_dir | md5)"
-  done
-  declare -r tmp_dir
-  mkdir -p "$tmp_dir"
-  cd "$tmp_dir"
+# Download, extract, and execute the installer
+curl -O "$TEXLIVE_TLNET_URL/install-tl-unx.tar.gz"
+tar xvf "install-tl-unx.tar.gz"
+cd "install-tl-$(TZ=UTC date '+%Y%m%d')"
+./install-tl \
+  --repository="$TEXLIVE_TLNET_URL" \
+  --profile="$REPO_ROOT/config/texlive.profile"
 
-  # Download, extract, and execute the installer
-  curl -O "$texlive_tlnet_url/install-tl-unx.tar.gz"
-  tar xvf "install-tl-unx.tar.gz"
-  cd "install-tl-$(TZ=UTC date '+%Y%m%d')"
-  ./install-tl \
-    --repository="$texlive_tlnet_url" \
-    --profile="$REPO_ROOT/config/texlive.profile"
-
-  # Clean up the tmp dir
-  cd "$REPO_ROOT"
-  rm -r "$tmp_dir"
-}
-
-install_texlive "$TEXLIVE_TLNET_URL"
+# Remove the tmp dir
+cd "$REPO_ROOT"
+rm -r "$TMP_DIR"
