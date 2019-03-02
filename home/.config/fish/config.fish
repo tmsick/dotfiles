800e522170269fbe557f2f0a081dee99ba83cc4f
Tomoya Yoshioka <tmsick.jp@gmail.com>
2019-03-03 04:21:17 +0900

    Revert "Remove rbenv initializer"
    This reverts commit 1c1ed69ef44403087b4c8b74a1dafcad75c14fd3.

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -6,5 +6,10 @@ set -g fish_user_paths $HOME/bin $fish_user_paths
 # GitHub's hub (https://github.com/github/hub)
 eval ( hub alias -s )
 
+# rbenv (https://github.com/rbenv/rbenv)
+if type -q rbenv
+  status --is-interactive; and source (rbenv init -|psub)
+end
+
 # Dan Kogai's mv2trash (https://github.com/dankogai/osx-mv2trash)
 alias rm mv2trash
