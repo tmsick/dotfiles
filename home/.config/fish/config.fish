4ee4486a5da8946f0dd62f677a507fe4bd4c55d5
Tomoya Yoshioka <tmsick.me@gmail.com>
2019-05-16 04:58:19 +0900

    Add $HOME/.cargo/bin to $fish_user_paths

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -6,7 +6,7 @@ set -g fish_user_paths /usr/local/sbin $fish_user_paths
 set -g fish_user_paths /usr/local/opt/ruby/bin $fish_user_paths
 set -g fish_user_paths $HOME/bin $fish_user_paths
 set -g fish_user_paths $GOPATH/bin $fish_user_paths
-
+set -g fish_user_paths $HOME/.cargo/bin $fish_user_paths
 
 # GitHub's hub (https://github.com/github/hub)
 eval ( hub alias -s )
