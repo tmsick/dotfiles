fc6e54e8a060bb0e6132d084f7a6753acc70a3ad
Tomoya Yoshioka <tmsick.me@gmail.com>
2019-07-21 21:46:45 +0900

    Refine app/commandrunner.py

--- app/commandrunner.py
+++ app/commandrunner.py
@@ -1,37 +1,35 @@
 class CommandRunner:
     @staticmethod
     def get_optimized_env():
-        def traverse(rootpath, paths, indices):
-            from pathlib import Path
-
-            cwd = Path.cwd()
+        def traverse(path):
             for i, p in enumerate(paths):
-                if p.samefile(rootpath) and (not p.samefile(cwd)):
+                if p.samefile(path) and (not p.samefile(cwd)):
                     indices.add(i)
-            for p in rootpath.iterdir():
+            for p in path.iterdir():
                 if p.is_dir():
-                    traverse(p, paths, indices)
+                    traverse(p)
 
         import os
         from pathlib import Path
         from .path import root
 
         rootpath = root()
-        environ = os.environ
-        path = environ["PATH"]
+        env = os.environ
+        path = env["PATH"]
         paths = [Path(p.strip()) for p in path.split(":")]
         indices = set()
-        traverse(rootpath, paths, indices)
+        cwd = Path.cwd()
+        traverse(rootpath)
         indices = list(indices)
         for i in sorted(indices, reverse=True):
             paths.pop(i)
-        environ["PATH"] = ":".join([str(p).strip() for p in paths])
-        return environ
+        env["PATH"] = ":".join([str(p).strip() for p in paths])
+        return env
 
     def __init__(self):
-        self.env = CommandRunner.get_optimized_env()
+        self.__env = CommandRunner.get_optimized_env()
 
     def run(self, args: list):
         from subprocess import run, PIPE
 
-        return run(args, stdout=PIPE, stderr=PIPE, env=self.env)
+        return run(args, stdout=PIPE, stderr=PIPE, env=self.__env)
