07d9e5069c9fec0e4da1844c7b0949299e332e43
Tomoya Yoshioka <tmsick.me@gmail.com>
2019-08-15 07:21:04 +0900

    Update home/.config/fish/config.fish
    Remove /usr/local/go/bin from $fish_user_paths. Instead, added /etc/paths.d/go whose content is "/usr/local/go/bin".

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,15 +1,14 @@
-set -gx LANG en_US.UTF-8
+set -gx LANG "en_US.UTF-8"
 set -gx PIPENV_VENV_IN_PROJECT 1
-set -gx GOPATH $HOME/go
+set -gx GOPATH "$HOME/go"
 
-set -g fish_user_paths /usr/local/sbin $fish_user_paths
-set -g fish_user_paths $HOME/bin $fish_user_paths
-set -g fish_user_paths /usr/local/go/bin $fish_user_paths
-set -g fish_user_paths $GOPATH/bin $fish_user_paths
-set -g fish_user_paths $HOME/.cargo/bin $fish_user_paths
+set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
+set -g fish_user_paths "$HOME/bin" $fish_user_paths
+set -g fish_user_paths "$GOPATH/bin" $fish_user_paths
+set -g fish_user_paths "$HOME/.cargo/bin" $fish_user_paths
 
 # GitHub's hub (https://github.com/github/hub)
-eval ( hub alias -s )
+eval (hub alias -s)
 
 # rbenv (https://github.com/rbenv/rbenv)
 status --is-interactive
