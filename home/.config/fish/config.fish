04aae4f17488f0bcbfd2f984c85ae22ce8baf596
Tomoya Yoshioka <tmsick.me@gmail.com>
2019-08-22 16:16:02 +0900

    Update home/.config/fish/config.fish

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,13 +1,27 @@
 set -gx LANG "en_US.UTF-8"
-set -gx PIPENV_VENV_IN_PROJECT "1"
+set -gx HOMEBREW_PREFIX "/usr/local"
+set -gx HOMEBREW_CELLAR "/usr/local/Cellar"
+set -gx HOMEBREW_REPOSITORY "/usr/local/Homebrew"
 set -gx GOPATH "$HOME/go"
+set -gx PIPENV_VENV_IN_PROJECT "1"
 set -gx FZF_DEFAULT_OPTS "--ansi"
 
-set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
-set -g fish_user_paths "$HOME/bin" $fish_user_paths
-set -g fish_user_paths "/usr/local/opt/curl/bin" $fish_user_paths
-set -g fish_user_paths "$GOPATH/bin" $fish_user_paths
-set -g fish_user_paths "$HOME/.cargo/bin" $fish_user_paths
+set -q MANPATH
+or set MANPATH ''
+set -gx MANPATH "/usr/local/share/man" $MANPATH
+
+set -q INFOPATH
+or set INFOPATH ''
+set -gx INFOPATH "/usr/local/share/info" $INFOPATH
+
+set -g fish_user_paths \
+    "$HOME/bin" \
+    "$GOPATH/bin" \
+    "$HOME/.cargo/bin" \
+    "/usr/local/opt/curl/bin" \
+    "/usr/local/bin" \
+    "/usr/local/sbin" \
+    $fish_user_paths
 
 # GitHub's hub (https://github.com/github/hub)
 eval (hub alias -s)
