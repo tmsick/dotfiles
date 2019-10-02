58aa008d092eca0648b1d2a48b4eea5e99a35a5d
Tomoya Yoshioka <tomoya.y@icloud.com>
2019-10-02 21:01:10 +0900

    Adjust symlink.sh

--- symlink.sh
+++ symlink.sh
@@ -22,7 +22,7 @@ function warn() {
 }
 
 function symlink() {
-    local status="0"
+    local status=0
 
     local src="$1"
     local dist="$2"
@@ -35,19 +35,16 @@ function symlink() {
 
         if [[ -f "$src/$item" ]]; then
             if [[ -e "$dist/$item" ]]; then
-                if [[ -L "$dist/$item" ]] &&
-                    [[ "$dist/$item" -ef "$src/$item" ]]; then
+                if [[ -L "$dist/$item" ]] && [[ "$dist/$item" -ef "$src/$item" ]]; then
                     : # already linked and do nothing
                 else
-                    warn "'$dist/$item' already exists." \
-                        "Skip linking '$src/$item'"
-                    status="1"
+                    warn "'$dist/$item' already exists. Skip linking '$src/$item'"
+                    status=1
                 fi
             else
                 if [[ -e "$dist" ]] && [[ ! -d "$dist" ]]; then
-                    warn "'$dist', which is not a directory, exists." \
-                        "Skip linking files under '$src'"
-                    return "1"
+                    warn "'$dist', which is not a directory, exists. Skip linking files under '$src'"
+                    return 1
                 fi
 
                 mkdir -p "$dist"
@@ -55,20 +52,18 @@ function symlink() {
             fi
         elif [[ -d "$src/$item" ]]; then
             if ! symlink "$src/$item" "$dist/$item"; then
-                status="1"
+                status=1
             fi
         fi
     done
 
-    return "$status"
+    return $status
 }
 
 if symlink "$DIR_ABS/home" "$HOME"; then
-    success "Successfully linked all the dotfiles." \
-        'Now, consider running `fisher` to install Fisher packages.'
-    exit "0"
+    success "Successfully linked all the dotfiles. Now, consider running $(fisher) to install Fisher packages."
+    exit 0
 else
-    warn "There was a problem(s) linking some dotfiles." \
-        "Consult warnings written above and solve them."
-    exit "1"
+    warn "There was a problem(s) linking some dotfiles. Consult warnings written above and solve them."
+    exit 1
 fi
