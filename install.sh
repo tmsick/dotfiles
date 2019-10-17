bf842162f9803bcd848ce4833f84f975a30f0b91
Tomoya Yoshioka <tomoya.y@icloud.com>
2019-10-17 20:30:47 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,41 +8,48 @@ readonly DIR_ABS="$PWD/$DIR"
 readonly FILE_ABS="$PWD/$DIR/$FILE"
 
 readonly XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-"$HOME/.config"}"
-
-readonly RESET="$(printf '\033[m')"
-readonly YELLOW="$(printf '\033[33m')"
-
-function warn() {
-    echo $YELLOW"$@"$RESET >&2
-}
-
-((status = 0))
-
-# Install Homebrew/Cask formulae and Mac App Store apps based on Brewfile
 readonly BREWFILE_PATH="$DIR_ABS/Brewfile"
-if ! (brew analytics off && brew update && brew bundle --path="$BREWFILE_PATH" && brew cleanup && brew doctor); then
-    warn "Failed to bundle brew. Try manually running \`brew bundle --path='$BREWFILE_PATH'\`"
-    ((status += 2 ** 0))
-fi
-
-# Install Fisher (https://github.com/jorgebucaran/fisher)
 readonly FISHER_PATH="$XDG_CONFIG_HOME/fish/functions/fisher.fish"
-if ! curl "https://git.io/fisher" --create-dirs -sLo "$FISHER_PATH"; then
-    warn "Failed to install Fisher."
-    ((status += 2 ** 1))
-fi
-
-# Initialize fzf (https://github.com/junegunn/fzf)
 readonly FZF_BIN="/usr/local/opt/fzf"
 readonly FZF_INIT_SCRIPT="$FZF_BIN/install"
-if [[ -x "$FZF_INIT_SCRIPT" ]]; then
-    if ! "$FZF_INIT_SCRIPT" --xdg --key-bindings --completion --no-update-rc --no-bash --no-zsh; then
-        warn "Failed to initialize fzf. Try manually running \`$FZF_INIT_SCRIPT\`"
-        ((status += 2 ** 2))
-    fi
-else
-    warn "Executable '$FZF_INIT_SCRIPT' not exists. Examine 'https://github.com/junegunn/fzf' or re-install fzf and follow the instructions to finish installing fzf key bindings and completions."
-    ((status += 2 ** 3))
-fi
 
-exit $status
+function warn() {
+    local reset="$(printf '\033[m')"
+    local yellow="$(printf '\033[33m')"
+
+    echo $yellow"$@"$reset >&2
+}
+
+function init_brew() {
+    if ! (brew analytics off && brew update && brew bundle --path="$BREWFILE_PATH" && brew cleanup && brew doctor); then
+        warn "Failed to bundle brew. Try manually running \`brew bundle --path='$BREWFILE_PATH'\`"
+        return 1
+    fi
+}
+
+function install_fisher() {
+    if ! curl "https://git.io/fisher" --create-dirs -sLo "$FISHER_PATH"; then
+        warn "Failed to install Fisher."
+        return 1
+    fi
+}
+
+function init_fzf() {
+    if [[ -x "$FZF_INIT_SCRIPT" ]]; then
+        if ! "$FZF_INIT_SCRIPT" --xdg --key-bindings --completion --no-update-rc --no-bash --no-zsh; then
+            warn "Failed to initialize fzf. Try manually running \`$FZF_INIT_SCRIPT\`"
+            return 1
+        fi
+    else
+        warn "Executable '$FZF_INIT_SCRIPT' not exists. Examine 'https://github.com/junegunn/fzf' or re-install fzf and follow the instructions to finish installing fzf key bindings and completions."
+        return 1
+    fi
+}
+
+function main() {
+    init_brew
+    install_fisher
+    init_fzf
+}
+
+main >/dev/null
