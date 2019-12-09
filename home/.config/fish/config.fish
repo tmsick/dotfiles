c5eb64bd761c11fdf5b32ed8cba4d47f8cf7c2ed
Tomoya Yoshioka <tomoya.y@icloud.com>
2019-12-09 14:55:57 +0900

    Set env var HOMEBREW_NO_ANALYTICS to 1

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -4,6 +4,7 @@ set -gx DOTFILES_HOME "$HOME/.dotfiles"
 set -gx HOMEBREW_PREFIX "/usr/local"
 set -gx HOMEBREW_CELLAR "/usr/local/Cellar"
 set -gx HOMEBREW_REPOSITORY "/usr/local/Homebrew"
+set -gx HOMEBREW_NO_ANALYTICS 1
 set -gx GOPATH "$HOME/go"
 set -gx DOCKER_HIDE_LEGACY_COMMANDS 1
 set -gx PIPENV_VENV_IN_PROJECT 1
