db35ce7efde2e0da37efa5df87c703263396fb3d
Tomoya Yoshioka <tomoya.y@icloud.com>
2020-02-15 04:08:27 +0900

    Update snippets/bash-script-header.sh

--- snippets/bash-script-header.sh
+++ snippets/bash-script-header.sh
@@ -2,26 +2,26 @@
 
 set -Ceu -o pipefail
 
+# ============================== PATH VARIABLES ================================
 readonly SCRIPT_NAME="$(basename "$0")"
 readonly SCRIPT_DIR="$(dirname "$0")"
 readonly SCRIPT_NAME_ABS="$PWD/$SCRIPT_DIR/$SCRIPT_NAME"
 readonly SCRIPT_DIR_ABS="$PWD/$SCRIPT_DIR"
 
-readonly GREEN="$(printf '\033[32m')"
-readonly YELLOW="$(printf '\033[33m')"
-readonly RED="$(printf '\033[31m')"
-readonly RESET="$(printf '\033[m')"
+# ============================= PRINT FUNCTIONS ================================
+readonly __COLOR_SWITCH_GREEN__="$(printf '\033[32m')"
+readonly __COLOR_SWITCH_YELLOW__="$(printf '\033[33m')"
+readonly __COLOR_SWITCH_RED__="$(printf '\033[31m')"
+readonly __COLOR_SWITCH_RESET__="$(printf '\033[m')"
 
 function success() {
-    echo $GREEN"$@"$RESET
+    echo $__COLOR_SWITCH_GREEN__"$@"$__COLOR_SWITCH_RESET__
 }
-
 function warn() {
-    echo $YELLOW"$@"$RESET >&2
+    echo $__COLOR_SWITCH_YELLOW__"$@"$__COLOR_SWITCH_RESET__ >&2
 }
-
 function error() {
-    echo $RED"$@"$RESET >&2
+    echo $__COLOR_SWITCH_RED__"$@"$__COLOR_SWITCH_RESET__ >&2
 }
 
 # ============================== END OF HEADER =================================
