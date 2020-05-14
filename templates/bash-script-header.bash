52d1b595ed1024253b54ed7392bac19c55cecdb6
Tomoya Yoshioka <tomoya.y@icloud.com>
2020-05-15 02:48:03 +0900

    Fix typo

--- templates/bash-script-header.bash
+++ templates/bash-script-header.bash
@@ -8,19 +8,19 @@ set -o errexit \
 readonly SCRIPT_NAME="${0##*/}"
 readonly SCRIPT_DIR=$(cd "${0%/*}" && pwd)
 
-readonly __asni_reset__=$(printf '\033[0m')
-readonly __asni_fg_red__=$(printf '\033[31m')
-readonly __asni_fg_green__=$(printf '\033[32m')
-readonly __asni_fg_yellow__=$(printf '\033[33m')
+readonly __ansi_reset__=$(printf '\033[0m')
+readonly __ansi_fg_red__=$(printf '\033[31m')
+readonly __ansi_fg_green__=$(printf '\033[32m')
+readonly __ansi_fg_yellow__=$(printf '\033[33m')
 
 success() {
-    echo "$__asni_fg_green__""$*""$__asni_reset__"
+    echo "$__ansi_fg_green__""$*""$__ansi_reset__"
 }
 
 warn() {
-    echo "$__asni_fg_yellow__""$*""$__asni_reset__" >&2
+    echo "$__ansi_fg_yellow__""$*""$__ansi_reset__" >&2
 }
 
 error() {
-    echo "$__asni_fg_red__""$*""$__asni_reset__" >&2
+    echo "$__ansi_fg_red__""$*""$__ansi_reset__" >&2
 }
