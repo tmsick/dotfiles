cb6c4dcd7d643fa65cc6b6b8bee0450e59ab58fa
Tomoya Yoshioka <tomoya.y@icloud.com>
2024-03-31 10:17:51 +0900

    Update config.fish

--- __home__/.config/fish/config.fish
+++ __home__/.config/fish/config.fish
@@ -1,32 +1,35 @@
 if status is-interactive
-    eval (/usr/local/bin/brew shellenv)
+    eval (/opt/homebrew/bin/brew shellenv)
 
     set -x XDG_CONFIG_HOME "$HOME/.config"
+
+    set -x GOHOME "$HOME/go"
+    set -x GOBIN "$GOHOME/bin"
 
     set -x BAT_THEME Dracula
     set -x CLICOLOR 1
     set -x DOCKER_HIDE_LEGACY_COMMANDS 1
     set -x DOTFILES_HOME "$HOME/.dotfiles"
     set -x EDITOR vim
+    set -x HOMEBREW_BUNDLE_FILE "$XDG_CONFIG_HOME/brew/Brewfile"
     set -x LANG "en_US.UTF-8"
     set -x PIPENV_VENV_IN_PROJECT 1
     set -x PIPENV_VERBOSITY -1
+    set -x PIPX_HOME "$HOME/.pipx"
     set -x POETRY_VIRTUALENVS_IN_PROJECT 1
     set -x PYENV_ROOT "$HOME/.pyenv"
-    set -x HOMEBREW_BUNDLE_FILE "$XDG_CONFIG_HOME/brew/Brewfile"
 
     set -a fish_function_path "$DOTFILES_HOME/fish/functions"
 
-    fish_add_path -pm "$HOMEBREW_PREFIX/sbin"
+    fish_add_path -pm "$GOBIN"
     fish_add_path -pm "$HOME/.local/bin"
-    fish_add_path -pm "$PYENV_ROOT/bin"
-    fish_add_path -pm "$HOME/go/bin"
+    fish_add_path -pm "$HOME/bin"
     fish_add_path -pm "$HOMEBREW_PREFIX/opt/fzf/bin"
 
-    which -s less && alias less "less -i"
-    which -s nvim && alias vim nvim
-    which -s bat && alias cat bat
-    which -s grpcurl && alias gcurl grpcurl
+    command -q bat && alias cat bat
+    command -q grpcurl && alias gcurl grpcurl
+    command -q less && alias less "less -i"
+    command -q nvim && alias vim nvim
 
     # Starship (https://starship.rs)
     starship init fish | source
