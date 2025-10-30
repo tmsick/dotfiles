1b69cc5453b553c624da7dbacfaf9352fe7d5285
Tomoya Yoshioka <tomoya.y@icloud.com>
2025-10-31 02:33:31 +0900

    Fix config.fish

--- __home__/.config/fish/config.fish
+++ __home__/.config/fish/config.fish
@@ -14,12 +14,12 @@ set -gx PNPM_HOME "$HOME/Library/pnpm"
 
 set -a fish_function_path "$DOTFILES_HOME/fish/functions"
 
+fish_add_path -gpm "$HOMEBREW_PREFIX/opt/fzf/bin"
+fish_add_path -gpm "$PNPM_HOME"
 fish_add_path -gpm "$HOME/.local/bin"
 fish_add_path -gpm "$HOME/.local/share/mise/shims"
 fish_add_path -gpm "$HOME/bin"
 fish_add_path -gpm "$HOME/go/bin"
-fish_add_path -gpm "$HOMEBREW_PREFIX/opt/fzf/bin"
-fish_add_path -gpm "$PNPM_HOME"
 
 command -q bat && alias cat bat
 command -q git && alias g git
